5fb4e84b25d34fb67997ca6d962d83a0c8bbbb8d
Brian Sisney <brian.sisney@nytimes.com>
2024-04-26 16:51:22 -0400

    add dev as prefix

--- dev/main.tf
+++ dev/main.tf
@@ -1,3 +1,7 @@
 module "dev" {
     source = "../modules/blog"
+    environment = {
+      name = "dev"
+      network_prefix = "10.1"
+    }
 }
--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -2,3 +2,16 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "environment" {
+  description = "Deployment environment"
+
+  type        = object ({
+    name           = string
+    network_prefix = string
+  })
+  default = {
+    name           = "what"
+    network_prefix = "10.0"
+  }
+}
